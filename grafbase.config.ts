f46c0451d2a8e142e3935db774e5969cdd825e34
arzukarla2 <65809748+arzukarla2@users.noreply.github.com>
2024-02-01 20:19:59 +0000

    first commit

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,4 +1,10 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { g, config } from '@grafbase/sdk'
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+
 
 // @ts-ignore
 const User = g.model('User', {
@@ -9,10 +15,7 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
 })
-
 // @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
@@ -22,21 +25,9 @@ const Project = g.model('Project', {
   githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
 })
-
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
-
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
-})
+  //
 
+})
